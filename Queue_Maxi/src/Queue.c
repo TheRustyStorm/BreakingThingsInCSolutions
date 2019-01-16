8cfdb8b6330be8fa150c3b57ea54137f83acb9ce
TheSovietStorm <TheSovietStorm@users.noreply.github.com>
2019-01-16 16:22:24 +0100

    Update Queue.c

--- Queue_Maxi/src/Queue.c
+++ Queue_Maxi/src/Queue.c
@@ -10,14 +10,16 @@ void queue_create(queue** q, int maxSize, void* (*allocation)(size_t)){
 
 void enqueue(queue* q, int value){
     if(q->size < q->maxSize){
-        q->memory[q->maxSize - q->size - 1] = value;
+        q->memory[q->size] = value;
         q->size++;
     }
 }
 
 void dequeue(queue* q){
     if(q->size > 0){
-        q->memory[q->maxSize - q->size - 1] = q->memory[q->maxSize - q->size - 2];
+        for(int i = 0; i < q->size; i++){
+            q->memory[i] = q->memory[i+1];    
+        }
         q->size--;
     }
 }
